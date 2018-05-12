47a0747da8b202242db827cd895584b243388ae2
Cullen Murphy <mac.man25@gmail.com>
2018-05-12 02:06:44 -0500

    Updated install script

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-wget http://www.ant.uni-bremen.de/whomes/rinas/nullmodem/download/nullmodem-0.0.6.tar.gz
+# wget http://www.ant.uni-bremen.de/whomes/rinas/nullmodem/download/nullmodem-0.0.6.tar.gz
 tar xaf nullmodem-0.0.6.tar.gz
 cd nullmodem-0.0.6
 ./configure
 make
 sudo make install
 
-sudo apt install socat
+sudo apt install -y socat
+
+cd ..
 sudo cp 01-ttyACM.rules /etc/udev/rules.d/
 sudo cp attach-tty@.service /lib/systemd/system/
 sudo cp nullmodem.service /lib/systemd/system
 
 sudo systemctl daemon-reload
-
+echo "OK, plug in a couple photons that like serial ports :)"
